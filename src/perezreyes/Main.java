b2a6141bb27fb6fa159246ec693dfe136587c9e0
Raymondo97 <jayden.thomas.b@gmail.com>
2021-10-06 20:01:50 -0600

    Spelling fix and message added

--- src/perezreyes/Main.java
+++ src/perezreyes/Main.java
@@ -3,6 +3,7 @@ package perezreyes;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hellow my name is Nefi");
+	System.out.println("Hello my name is Nefi");
+    System.out.println("Hello Nefi! My name is Jayden!!");
     }
 }
